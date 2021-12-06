9af6758f827dcf1ac38e5b0ef0ebf6a271ae5a7a
m01otov <67689912+m01otov@users.noreply.github.com>
2021-12-06 21:08:03 +0500

    Create LibraryList and Model Library

---
+++ AppleMusicSwiftUI/Model/Library.swift
@@ -0,0 +1,32 @@
+//
+//  Library.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ilya Melnikov on 06.12.2021.
+//
+
+import SwiftUI
+
+struct Library:Hashable, Identifiable  {
+    var id = UUID()
+    var name: String
+
+    var iconName: String
+    var icon: Image {
+        Image(systemName: iconName)
+    }
+}
+
+var libraryList: [Library] = [
+    Library(name: "Плейлисты", iconName: "music.note.list"),
+    Library(name: "Артисты", iconName: "music.mic"),
+    Library(name: "Альбомы", iconName: "square.stack"),
+    Library(name: "Песни", iconName: "music.note"),
+    Library(name: "Телешоу и фильмы", iconName: "tv"),
+    Library(name: "Видеоклипы", iconName: "music.note.tv"),
+    Library(name: "Жанры", iconName: "guitars"),
+    Library(name: "Сборники", iconName: "person.2.crop.square.stack"),
+    Library(name: "Авторы", iconName: "music.quarternote.3"),
+    Library(name: "Загружено", iconName: "arrow.down.circle"),
+    Library(name: "Домашняя коллекция", iconName: "music.note.house"),
+]
---
+++ AppleMusicSwiftUI/View/LibraryList.swift
@@ -0,0 +1,29 @@
+//
+//  LibraryList.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ilya Melnikov on 06.12.2021.
+//
+
+import SwiftUI
+
+struct LibraryList: View {
+
+    //Для идентификации типов, интерфейсов и других элементов
+    @State private var multiSelection = Set<UUID>()
+
+    var body: some View {
+        List(selection: $multiSelection) {
+            ForEach(libraryList, id: \.self) { library in
+                Text("name") // add cell
+        }
+            .onMove(perform: { _,_ in })
+        }
+    }
+}
+
+struct LibraryList_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryList()
+    }
+}
