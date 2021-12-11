c6b15fe376548b6f78c0a1b292e7f2ee33dee2cb
m01otov <67689912+m01otov@users.noreply.github.com>
2021-12-11 14:31:23 +0500

    Add radioView and Cell

---
+++ AppleMusicSwiftUI/View/RadioView.swift
@@ -0,0 +1,24 @@
+//
+//  RadioView.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ilya Melnikov on 11.12.2021.
+//
+
+import SwiftUI
+
+struct RadioView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView{
+
+            }
+        }
+    }
+}
+
+struct RadioView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioView()
+    }
+}
