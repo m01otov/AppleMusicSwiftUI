c601a5a32381739908b33f41676d50b1933671eb
m01otov <67689912+m01otov@users.noreply.github.com>
2021-12-22 21:48:37 +0500

    Add logic in SearchView

--- AppleMusicSwiftUI/View/SearchView.swift.swift
+++ AppleMusicSwiftUI/View/SearchView.swift.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
+
 let columns = [
     GridItem(.flexible()),
     GridItem(.flexible())
 ]
 
 struct SearchView_swift: View {
+
+    @ObservedObject var barSearchList = ContainerBar()
+
+    @State private var searchPlaceholder: String = "Ваша медиатека"
+
+    @State private var selectedSearch = 1
+
     var body: some View {
         NavigationView{
             ScrollView{
@@ -32,10 +40,39 @@ struct SearchView_swift: View {
                 .padding(.horizontal, 20)
             }
             .navigationTitle("Поиск")
+            .searchable(text: $barSearchList.searchText,
+                        placement: .navigationBarDrawer(displayMode: .always),
+                        prompt: self.$searchPlaceholder.wrappedValue) {
+                VStack(alignment: .leading, spacing: 15){
+                    Picker("SearchSource", selection: $selectedSearch) {
+                        Text("Apple Music").tag(0)
+                        Text("Ваша медиатека").tag(1)
+                    }
+                    .onChange(of: selectedSearch) { tag in
+                        if tag == 0 {
+                            barSearchList.getAppleMusicData()
+                            searchPlaceholder = "Артисты, песни, тексты и др."
+                        } else {
+                            barSearchList.getOwnData()
+                            searchPlaceholder = "Ваша медиатека"
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    ScrollView {
+                        LazyVGrid(columns: [GridItem(.flexible())], alignment: .leading ){
+                            ForEach(barSearchList.filterBars, id: \.self) {
+                                bar in ListBarViewCell(bar: bar)
+                                    .searchCompletion(bar.title)
+                                    .tint(.primary)
+                        }
+                    }
+                }
+            }
+
         }
     }
+  }
 }
-
 struct SearchView_swift_Previews: PreviewProvider {
     static var previews: some View {
         SearchView_swift()
